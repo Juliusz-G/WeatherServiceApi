58c19d47d4faea1a2abc715981a4e3bffff74727
Juliusz Gorzen <julek.gorzen@gmail.com>
2022-03-17 13:31:08 +0100

    replace entity class with new one

---
+++ src/main/java/model/entity/WeatherEntity.java
@@ -0,0 +1,42 @@
+package model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "weather_entity")
+public class WeatherEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "weather_id")
+    private int weatherId;
+
+    @Column(name = "city_name")
+    private String cityName;
+
+    @Column(name = "date")
+    private Timestamp date;
+
+    @Column(name = "temp")
+    private double temp;
+
+    @Column(name = "humidity")
+    private int humidity;
+
+    @Column(name = "pressure")
+    private int pressure;
+
+    @Column(name = "wind_deg")
+    private int windDeg;
+
+    @Column(name = "wind_speed")
+    private double windSpeed;
+}
