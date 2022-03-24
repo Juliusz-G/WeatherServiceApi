c4a34adbb5cd158793b8a23759f28f05625a6c59
Juliusz Gorzen <julek.gorzen@gmail.com>
2022-03-24 15:46:23 +0100

    WeatherService refactor

--- src/main/java/service/WeatherService.java
+++ src/main/java/service/WeatherService.java
@@ -1,6 +1,5 @@
 package service;
 
-
 import dao.WeatherDao;
 import lombok.Data;
 import model.api.WeatherApi;
@@ -18,7 +17,6 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-
 @Data
 public class WeatherService {
 
@@ -27,7 +25,8 @@ public class WeatherService {
     private final WeatherTransformer weatherTransformer;
     private final WeatherValidator weatherValidator;
 
-    public WeatherService(WeatherRepository weatherRepository, WeatherDao weatherDao, WeatherTransformer weatherTransformer, WeatherValidator weatherValidator) {
+    public WeatherService(WeatherRepository weatherRepository, WeatherDao weatherDao,
+                          WeatherTransformer weatherTransformer, WeatherValidator weatherValidator) {
         this.weatherRepository = weatherRepository;
         this.weatherDao = weatherDao;
         this.weatherTransformer = weatherTransformer;
@@ -65,7 +64,8 @@ public class WeatherService {
 
     public void addToDatabase(WeatherApi weatherApi, String date) {
         LocalDate localDate = stringToLocalDate(date);
-        weatherApi.getList().stream()
+        weatherApi.getList()
+                .stream()
                 .filter(weather -> epochToLocalDate(weather.getDt()).equals(localDate))
                 .map(weather -> weatherTransformer.fromApiToEntity(weather, weatherApi.getCity()))
                 .forEach(weatherDao::save);
@@ -73,18 +73,17 @@ public class WeatherService {
 
     public void updateWeatherForGivenCity(String cityName) {
         weatherDao.findByCity(cityName)
-                .forEach(weatherDao::update
-                );
+                .forEach(weatherDao::update);
     }
 
     public void deleteWeatherForGivenCity(String cityName) {
         weatherDao.findByCity(cityName)
-                .forEach(weatherDao::delete
-                );
+                .forEach(weatherDao::delete);
     }
 
     public List<WeatherDto> listAllWeathers() {
-        return weatherDao.findAllWeathers().stream()
+        return weatherDao.findAllWeathers()
+                .stream()
                 .map(weatherTransformer::fromEntityToDto)
                 .collect(Collectors.toList());
     }
@@ -95,7 +94,8 @@ public class WeatherService {
 
     public List<WeatherDto> findWeatherForGivenCity(String cityName) {
         if (weatherValidator.cityNameValidation(cityName)) {
-            return weatherDao.findByCity(cityName).stream()
+            return weatherDao.findByCity(cityName)
+                    .stream()
                     .map(weatherTransformer::fromEntityToDto)
                     .collect(Collectors.toList());
         }
@@ -104,7 +104,8 @@ public class WeatherService {
 
     public List<WeatherDto> findWeatherForGivenCityAndDate(String cityName, String resultDate) {
         if (weatherValidator.cityNameValidation(cityName)) {
-            return weatherDao.findByCityAndDate(cityName, resultDate).stream()
+            return weatherDao.findByCityAndDate(cityName, resultDate)
+                    .stream()
                     .map(weatherTransformer::fromEntityToDto)
                     .collect(Collectors.toList());
         }
@@ -113,38 +114,45 @@ public class WeatherService {
     }
 
     public List<WeatherDto> findWeatherForGivenCoordinatesAndDate(String lon, String lat, String date) {
-        String resultDate = weatherValidator.displayWeatherDateValidation(date) ? date : LocalDate.now().plusDays(1).format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
-        return weatherDao.findByCoordinatesAndDate(lon, lat, resultDate).stream()
+        String resultDate = weatherValidator.displayWeatherDateValidation(date) ? date : LocalDate.now()
+                .plusDays(1)
+                .format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+        return weatherDao.findByCoordinatesAndDate(lon, lat, resultDate)
+                .stream()
                 .map(weatherTransformer::fromEntityToDto)
                 .collect(Collectors.toList());
     }
 
     public List<WeatherDto> findAllByDate(String date) {
-        return weatherDao.findByDate(date).stream().map(weatherTransformer::fromEntityToDto).collect(Collectors.toList());
+        return weatherDao.findByDate(date)
+                .stream()
+                .map(weatherTransformer::fromEntityToDto)
+                .collect(Collectors.toList());
     }
 
     public LocalDate epochToLocalDate(Long epochDate) {
-        return Instant.ofEpochSecond(epochDate).atZone(ZoneId.systemDefault()).toLocalDate();
+        return Instant.ofEpochSecond(epochDate)
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
     }
 
-
     public LocalDate stringToLocalDate(String date) {
-        String resultDate = weatherValidator.addWeatherDateValidation(date) ? date : LocalDate.now().plusDays(1).format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+        String resultDate = weatherValidator.addWeatherDateValidation(date) ? date : LocalDate.now()
+                .plusDays(1)
+                .format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
         return LocalDate.parse(resultDate, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
     }
 
     public List<String> displayDistinctCityNames() {
-        return weatherDao.findAllWeathers().stream()
+        return weatherDao.findAllWeathers()
+                .stream()
                 .filter(distinctByKey(WeatherEntity::getCityName))
                 .map(WeatherEntity::getCityName)
                 .collect(Collectors.toList());
     }
 
-
     private <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
         Map<Object, Boolean> seen = new ConcurrentHashMap<>();
         return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
     }
-
-
 }
